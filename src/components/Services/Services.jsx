9c9d03c51f23173feffc9f58e2302e83f1d6e194
MD MOHABBAT ALI <mohabbatalit8@gmail.com>
2023-05-03 23:16:27 +0600

    Extra Section Services Added

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,21 +1,39 @@
 import React from 'react';
+import { FaBeer, FaCoffee,  FaLemon } from 'react-icons/fa';
+
 
 const Services = () => {
     return (
         <div>
             <h1 className='mt-24 mb-5 text-5xl font-bold'>Our Services</h1>
-            <h4 className='text-xl'>It is a long established fact that a reader will be distracted by the readable <br /> content of a page when looking at its layout.</h4>
-            <div>
+            <h4 className='text-xl mb-10'>It is a long established fact that a reader will be distracted by the readable <br /> content of a page when looking at its layout.</h4>
+            <div className='grid grid-cols-3 mb-24'>
                 <div className="card w-96 bg-base-100 shadow-xl">
                     <figure className="px-10 pt-10">
-                        <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+                    <FaCoffee className='text-6xl'></FaCoffee>
                     </figure>
                     <div className="card-body items-center text-center">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Buy Now</button>
-                        </div>
+                        <h2 className="card-title">Breakfast</h2>
+                        <p>It is a long established fact that a reader will be distracted by the readable content.</p> 
+                    </div>
+                </div>
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <figure className="px-10 pt-10">
+                    {/* <BiRestaurant></BiRestaurant> */}
+                    <FaLemon className='text-6xl'></FaLemon>
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Lunch</h2>
+                        <p>It is a long established fact that a reader will be distracted by the readable content.</p> 
+                    </div>
+                </div>
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <figure className="px-10 pt-10">
+                    <FaCoffee className='text-6xl'></FaCoffee>
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Dinner</h2>
+                        <p>It is a long established fact that a reader will be distracted by the readable content.</p> 
                     </div>
                 </div>
             </div>
