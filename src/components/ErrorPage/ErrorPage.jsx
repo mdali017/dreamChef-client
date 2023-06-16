b23ab377d6c395b2667abf5b60901a32e6538a09
MD MOHABBAT ALI <mohabbatalit8@gmail.com>
2023-06-16 15:56:07 +0600

    Error Element Added

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,20 +1,21 @@
 import { FaceFrownIcon } from '@heroicons/react/24/solid'
 import React from 'react'
 import { Link, useRouteError } from 'react-router-dom'
+import errorImg from '../../assets/ErrorElement/36395-lonely-404.json'
 
 const ErrorPage = () => {
   const { error, status } = useRouteError()
   return (
     <section className='flex items-center h-screen p-16 bg-gray-100 text-gray-900'>
       <div className='container flex flex-col items-center justify-center px-5 mx-auto my-8'>
-        <FaceFrownIcon className='w-40 h-40 text-yellow-500' />
+       <img src={errorImg} alt="" />
         <div className='max-w-md text-center'>
           <h2 className='mb-8 font-extrabold text-9xl text-green-500'>
             <span className='sr-only'>Error</span>
             <span className=''>{status || 404}</span>
           </h2>
           <p className='text-2xl font-semibold md:text-3xl text-red-800 mb-8'>
-            {error?.message}
+            Not Found Page
           </p>
           <Link to='/' className='btn'>
             Back to homepage
